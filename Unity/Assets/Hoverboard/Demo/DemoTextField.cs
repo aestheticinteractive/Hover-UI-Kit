5d21cc9f1dfebeab66a26f6faa04756d8c78515c
Zach Kinstner <zach@aestheticinteractive.com>
2015-03-12 13:18:28 -0400

    Increased the width of the demo's text area.

--- Unity/Assets/Hoverboard/Demo/DemoTextField.cs
+++ Unity/Assets/Hoverboard/Demo/DemoTextField.cs
@@ -14,6 +14,8 @@ namespace Hoverboard.Demo {
 		////////////////////////////////////////////////////////////////////////////////////////////////
 		/*--------------------------------------------------------------------------------------------*/
 		public void Awake() {
+			const int width = 8;
+
 			var lblObj = new GameObject("TextEntry");
 			lblObj.transform.SetParent(gameObject.transform, false);
 			lblObj.transform.localRotation = Quaternion.FromToRotation(Vector3.down, Vector3.back);
@@ -23,7 +25,7 @@ namespace Hoverboard.Demo {
 			lblBgObj.transform.SetParent(lblObj.transform, false);
 			lblBgObj.transform.localPosition = new Vector3(0, 0, 0.01f);
 			lblBgObj.transform.localRotation = Quaternion.FromToRotation(Vector3.up, Vector3.back);
-			lblBgObj.transform.localScale = new Vector3(6, 1, 1);
+			lblBgObj.transform.localScale = new Vector3(width, 1, 1);
 			lblBgObj.renderer.material = new Material(Shader.Find("Unlit/AlphaSelfIllum"));
 			lblBgObj.renderer.material.color = new Color(0.1f, 0.1f, 0.1f, 0.666f);
 			lblBgObj.renderer.material.renderQueue -= 300;
@@ -32,7 +34,7 @@ namespace Hoverboard.Demo {
 			green.a *= 0.5f;
 
 			vTextEntry = lblObj.AddComponent<UiLabel>();
-			vTextEntry.SetSize(6, 1);
+			vTextEntry.SetSize(width, 1);
 			vTextEntry.FontName = "Tahoma";
 			vTextEntry.FontSize = 60;
 			vTextEntry.Color = green;
