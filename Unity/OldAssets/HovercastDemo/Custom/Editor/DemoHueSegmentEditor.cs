b52d0395fd1f3d31023ca093f2a5138712122082
Zach Kinstner <zach@aestheticinteractive.com>
2015-03-27 17:09:19 -0400

    Added some additional demo assets to the "OldAssets".

---
+++ Unity/OldAssets/HovercastDemo/Custom/Editor/DemoHueSegmentEditor.cs
@@ -0,0 +1,20 @@
+using Hover.Demo.HovercastDemo.Custom;
+using UnityEditor;
+
+namespace Hovercast.Demo.Custom.Editor {
+
+	/*================================================================================================*/
+	[CustomEditor(typeof(DemoHueSegment))]
+	public class DemoHueSegmentEditor : UnityEditor.Editor {
+
+
+		////////////////////////////////////////////////////////////////////////////////////////////////
+		/*--------------------------------------------------------------------------------------------*/
+		public override void OnInspectorGUI() {
+			EditorGUILayout.HelpBox("This item automatically updates its color settings "+
+				"based on the current slider value.", MessageType.Info);
+		}
+
+	}
+
+}
