21a72644bca67984ace4a7090c8c137ea542adaa
Zach Kinstner <zach@aestheticinteractive.com>
2015-04-02 12:20:00 -0400

    Released version 1.0.0.

--- Core/Solution/Hover.Common/Properties/VersionInfo.cs
+++ Core/Solution/Hover.Common/Properties/VersionInfo.cs
@@ -1,4 +1,4 @@
 ﻿using System.Reflection;
 
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyInformationalVersion("1.0.0.922b901")]
+[assembly: AssemblyInformationalVersion("1.0.0.3e9eac5")]
