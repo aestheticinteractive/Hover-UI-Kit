b64249f49cab44383f8b7dfe9e6f702024c8dd45
Zach Kinstner <zach@aestheticinteractive.com>
2016-01-10 12:08:44 -0500

    Removed unused event delegates.

--- Core/Solution/Hover.Common/Items/ItemEvents.cs
+++ Core/Solution/Hover.Common/Items/ItemEvents.cs
@@ -5,12 +5,8 @@ namespace Hover.Common.Items {
 	/*================================================================================================*/
 	public static class ItemEvents {
 
-		public delegate void IsEnabledChangedHandler(IBaseItem pItem);
-		public delegate void IsVisibleChangedHandler(IBaseItem pItem);
-
 		public delegate void SelectedHandler(ISelectableItem pItem);
 		public delegate void DeselectedHandler(ISelectableItem pItem);
-
 		public delegate void ValueChangedHandler<T>(ISelectableItem<T> pItem);
 
 		public delegate void GroupItemSelectedHandler(IItemGroup pGroup, ISelectableItem pItem);
