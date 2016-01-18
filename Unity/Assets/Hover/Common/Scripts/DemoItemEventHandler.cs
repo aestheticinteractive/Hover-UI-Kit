28388afe146b591b7211358fc29e4b7d5ab254d5
Zach Kinstner <zach@aestheticinteractive.com>
2016-01-18 18:21:24 -0500

    Added more item event handlers to the test scene.

--- Unity/Assets/Hover/Common/Scripts/DemoItemEventHandler.cs
+++ Unity/Assets/Hover/Common/Scripts/DemoItemEventHandler.cs
@@ -1,4 +1,5 @@
 ﻿using Hover.Common.Items;
+using Hover.Common.Items.Groups;
 using UnityEngine;
 
 namespace Hover.Demo.Common {
@@ -6,26 +7,45 @@ namespace Hover.Demo.Common {
 	/*================================================================================================*/
 	public class DemoItemEventHandler : MonoBehaviour {
 
+		
+		////////////////////////////////////////////////////////////////////////////////////////////////
+		/*--------------------------------------------------------------------------------------------*/
+		public void HandleHierarchyLevelChanged(int pDirection) {
+			Debug.Log("Hierarchy Level Changed: Direction="+pDirection);
+		}
+		
+		/*--------------------------------------------------------------------------------------------*/
+		public void HandleHierarchyItemSelected(IItemGroup pGroup, ISelectableItem pItem) {
+			Debug.Log("Hierarchy Item Selected: GroupTitle="+pGroup.Title+", "+GetItemString(pItem));
+		}
+
 
 		////////////////////////////////////////////////////////////////////////////////////////////////
 		/*--------------------------------------------------------------------------------------------*/
 		public void HandleItemSelected(ISelectableItem pItem) {
-			Debug.Log("Item Selected: "+pItem.AutoId+" / "+pItem.Label);
+			Debug.Log("Item Selected: "+GetItemString(pItem));
 		}
 		
 		/*--------------------------------------------------------------------------------------------*/
 		public void HandleItemDeselected(ISelectableItem pItem) {
-			Debug.Log("Item Deselected: "+pItem.AutoId+" / "+pItem.Label);
+			Debug.Log("Item Deselected: "+GetItemString(pItem));
 		}
 		
 		/*--------------------------------------------------------------------------------------------*/
 		public void HandleItemValueChangedBool(ISelectableItem<bool> pItem) {
-			Debug.Log("Item Value Changed: "+pItem.AutoId+" / "+pItem.Label+" / "+pItem.Value);
+			Debug.Log("Item Value Changed: "+GetItemString(pItem)+", Value="+pItem.Value);
 		}
 		
 		/*--------------------------------------------------------------------------------------------*/
 		public void HandleItemValueChangedFloat(ISelectableItem<float> pItem) {
-			Debug.Log("Item Value Changed: "+pItem.AutoId+" / "+pItem.Label+" / "+pItem.Value);
+			Debug.Log("Item Value Changed: "+GetItemString(pItem)+", Value="+pItem.Value);
+		}
+		
+		
+		////////////////////////////////////////////////////////////////////////////////////////////////
+		/*--------------------------------------------------------------------------------------------*/
+		private string GetItemString(ISelectableItem pItem) {
+			return "AutoId="+pItem.AutoId+", Label="+pItem.Label;
 		}
 
 	}
