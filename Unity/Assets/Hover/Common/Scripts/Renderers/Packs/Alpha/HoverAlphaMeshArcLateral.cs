070a7d4dfb34847dd8eb73cdb2b59a4995a7fd59
Zach Kinstner <zach@aestheticinteractive.com>
2016-05-29 14:41:54 -0400

    Implemented new "arc" mesh classes.

---
+++ Unity/Assets/Hover/Common/Scripts/Renderers/Packs/Alpha/HoverAlphaMeshArcLateral.cs
@@ -0,0 +1,65 @@
+﻿using Hover.Common.Renderers.Shapes.Arc;
+using Hover.Common.Utils;
+using UnityEngine;
+
+namespace Hover.Common.Renderers.Packs.Alpha {
+
+	/*================================================================================================*/
+	public class HoverAlphaMeshArcLateral : HoverMeshArcButton {
+	
+		public const string AlphaName = "Alpha";
+
+		[DisableWhenControlled(RangeMin=0, RangeMax=1)]
+		public float Alpha = 1;
+
+		[DisableWhenControlled]
+		public Color FillColor = Color.gray;
+		
+		private Color vPrevColor;
+
+
+		////////////////////////////////////////////////////////////////////////////////////////////////
+		/*--------------------------------------------------------------------------------------------*/
+		public override void TreeUpdate() {
+			base.TreeUpdate();
+			UpdateColor();
+		}
+		
+		
+		////////////////////////////////////////////////////////////////////////////////////////////////
+		/*--------------------------------------------------------------------------------------------*/
+		protected override void UpdateMesh() {
+			//float uvToCenterX = (UseUvRelativeToSize ? 1-outerW/SizeX : 0);
+			//float uvToCenterY = (UseUvRelativeToSize ? 1-outerH/SizeY : 0);
+			float innerRad = Mathf.Lerp(InnerRadius, OuterRadius, InnerAmount);
+			float outerRad = Mathf.Lerp(InnerRadius, OuterRadius, OuterAmount);
+			float halfRadians = ArcAngle/180*Mathf.PI/2;
+			int steps = (int)Mathf.Max(2, ArcAngle/2);
+
+			MeshUtil.BuildRingMesh(vMeshBuild, innerRad, outerRad, -halfRadians, halfRadians, steps);
+			
+			/*for ( int i = 0 ; i < vMeshBuild.Uvs.Length ; i++ ) {
+				Vector2 uv = vMeshBuild.Uvs[i];
+				uv.x = Mathf.Lerp(uv.x, 0.5f, uvToCenterX);
+				uv.y = Mathf.Lerp(uv.y, 0.5f, uvToCenterY);
+				vMeshBuild.Uvs[i] = uv;
+			}*/
+			
+			vMeshBuild.Commit();
+		}
+		
+		/*--------------------------------------------------------------------------------------------*/
+		private void UpdateColor() {
+			Color color = DisplayUtil.FadeColor(FillColor, Alpha);
+
+			if ( !vForceUpdates && color == vPrevColor ) {
+				return;
+			}
+
+			vMeshBuild.CommitColors(color);
+			vPrevColor = color;
+		}
+		
+	}
+
+}
---
+++ Unity/Assets/Hover/Common/Scripts/Renderers/Shapes/Arc/HoverMeshArcButton.cs
@@ -0,0 +1,66 @@
+﻿using Hover.Common.Utils;
+
+namespace Hover.Common.Renderers.Shapes.Arc {
+
+	/*================================================================================================*/
+	public abstract class HoverMeshArcButton : HoverMesh {
+	
+		public const string OuterRadiusName = "OuterRadius";
+		public const string InnerRadiusName = "InnerRadius";
+		public const string ArcAngleName = "ArcAngle";
+		public const string OuterAmountName = "OuterAmount";
+		public const string InnerAmountName = "InnerAmount";
+		public const string UseUvRelativeToSizeName = "UseUvRelativeToSize";
+
+		[DisableWhenControlled(RangeMin=0, RangeMax=100)]
+		public float OuterRadius = 10;
+		
+		[DisableWhenControlled(RangeMin=0, RangeMax=100)]
+		public float InnerRadius = 3;
+
+		[DisableWhenControlled(RangeMin=0, RangeMax=360)]
+		public float ArcAngle = 60;
+		
+		[DisableWhenControlled(RangeMin=0, RangeMax=1)]
+		public float OuterAmount = 1;
+		
+		[DisableWhenControlled(RangeMin=0, RangeMax=1)]
+		public float InnerAmount = 0.5f;
+		
+		[DisableWhenControlled]
+		public bool UseUvRelativeToSize = false;
+
+		private float vPrevOuterRadius;
+		private float vPrevInnerRadius;
+		private float vPrevArcAngle;
+		private float vPrevInner;
+		private float vPrevOuter;
+		private bool vPrevUseUv;
+
+
+		////////////////////////////////////////////////////////////////////////////////////////////////
+		/*--------------------------------------------------------------------------------------------*/
+		protected override bool ShouldUpdateMesh() {
+			bool shouldUpdate = (
+				base.ShouldUpdateMesh() ||
+				OuterRadius != vPrevOuterRadius ||
+				InnerRadius != vPrevInnerRadius ||
+				ArcAngle != vPrevArcAngle ||
+				InnerAmount != vPrevInner ||
+				OuterAmount != vPrevOuter ||
+				UseUvRelativeToSize != vPrevUseUv
+			);
+
+			vPrevOuterRadius = OuterRadius;
+			vPrevInnerRadius = InnerRadius;
+			vPrevArcAngle = ArcAngle;
+			vPrevInner = InnerAmount;
+			vPrevOuter = OuterAmount;
+			vPrevUseUv = UseUvRelativeToSize;
+
+			return shouldUpdate;
+		}
+		
+	}
+
+}
