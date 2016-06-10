bc4dc04ed460aff0208a6cb4073c517945f2a09e
Zach Kinstner <zach@aestheticinteractive.com>
2016-06-09 23:58:02 -0400

    Implemented some Hovercast interface mechanics, including the ability to switch between rows upon item selection.

---
+++ Unity/Assets/Hover/Scripts/Interfaces/Cast/HovercastInterface.cs
@@ -0,0 +1,54 @@
+﻿using Hover.Layouts.Arc;
+using UnityEngine;
+
+namespace Hover.Interfaces.Cast {
+
+	/*================================================================================================*/
+	[ExecuteInEditMode]
+	public class HovercastInterface : MonoBehaviour {
+
+		public HoverLayoutArcStack ArcStack;
+		public HoverLayoutArcRow ActiveRow;
+		public HoverLayoutArcRow PreviousRow;
+
+
+		////////////////////////////////////////////////////////////////////////////////////////////////
+		/*--------------------------------------------------------------------------------------------*/
+		public void Awake() {
+			if ( ArcStack == null ) {
+				ArcStack = GetComponentInChildren<HoverLayoutArcStack>();
+			}
+
+			if ( ActiveRow == null ) {
+				ActiveRow = GetComponentInChildren<HoverLayoutArcRow>();
+			}
+		}
+
+
+		////////////////////////////////////////////////////////////////////////////////////////////////
+		/*--------------------------------------------------------------------------------------------*/
+		public void OnRowSwitched(HovercastRowSwitcher pSwitcher) { //via SendMessageUpwards()
+			if ( PreviousRow != null ) {
+				PreviousRow.gameObject.SetActive(false);
+			}
+
+			HoverLayoutArcRow targetRow =
+				(pSwitcher.UsePreviousActiveRow ? PreviousRow : pSwitcher.TargetRow);
+
+			if ( targetRow == null ) {
+				Debug.LogError("Could not transition to null/missing row.", this);
+				return;
+			}
+
+			//TODO: build transitions based on "pSwitcher.RowEntryTransition"
+
+			ActiveRow.gameObject.SetActive(false);
+			PreviousRow = ActiveRow;
+
+			ActiveRow = targetRow;
+			ActiveRow.gameObject.SetActive(true);
+		}
+
+	}
+
+}
---
+++ Unity/Assets/Hover/Scripts/Interfaces/Cast/HovercastRowSwitcher.cs
@@ -0,0 +1,35 @@
+﻿using Hover.Items;
+using Hover.Layouts.Arc;
+using UnityEngine;
+
+namespace Hover.Interfaces.Cast {
+
+	/*================================================================================================*/
+	[RequireComponent(typeof(SelectableItem))]
+	public class HovercastRowSwitcher : MonoBehaviour {
+		
+		public enum RowEntryTransitionType {
+			Immediate,
+			FromOutside,
+			FromInside
+		}
+
+		public HoverLayoutArcRow TargetRow;
+		public bool UsePreviousActiveRow = false;
+		public RowEntryTransitionType RowEntryTransition = RowEntryTransitionType.Immediate;
+
+
+		////////////////////////////////////////////////////////////////////////////////////////////////
+		/*--------------------------------------------------------------------------------------------*/
+		public void Start() {
+			GetComponent<SelectableItem>().OnSelectedEvent.AddListener(HandleItemSelected);
+		}
+
+		/*--------------------------------------------------------------------------------------------*/
+		private void HandleItemSelected(ISelectableItem pItem) {
+			SendMessageUpwards("OnRowSwitched", this, SendMessageOptions.RequireReceiver);
+		}
+
+	}
+
+}
