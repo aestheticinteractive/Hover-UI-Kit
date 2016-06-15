e5393469310f254ad649265f3e9f6143e4774dca
Zach Kinstner <zach@aestheticinteractive.com>
2016-06-14 20:11:53 -0400

    Added initial "child items" scripts to help modify groups of items.

---
+++ Unity/Assets/Hover/Scripts/Items/HoverChildItemsFinder.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections.Generic;
+using Hover.Utils;
+using UnityEngine;
+
+namespace Hover.Items {
+
+	/*================================================================================================*/
+	[ExecuteInEditMode]
+	[RequireComponent(typeof(TreeUpdater))]
+	public class HoverChildItemsFinder : MonoBehaviour, ITreeUpdateable {
+
+		public List<HoverItemData> ChildItems { get; private set; }
+
+		public bool FindOnlyImmediateChildren = false;
+		public bool ForceUpdate = false;
+
+		private bool vPrevAffectOnlyImmediateChildren;
+
+		
+		////////////////////////////////////////////////////////////////////////////////////////////////
+		/*--------------------------------------------------------------------------------------------*/
+		public void Start() {
+			//do nothing...
+		}
+		
+		/*--------------------------------------------------------------------------------------------*/
+		public void TreeUpdate() {
+			bool isFirst = false;
+
+			if ( ChildItems == null ) {
+				ChildItems = new List<HoverItemData>();
+				isFirst = true;
+			}
+
+			if ( !isFirst && !ForceUpdate && 
+					FindOnlyImmediateChildren == vPrevAffectOnlyImmediateChildren ) {
+				return;
+			}
+
+			ChildItems.Clear();
+			FillChildItemsList(gameObject.transform);
+
+			ForceUpdate = false;
+			vPrevAffectOnlyImmediateChildren = FindOnlyImmediateChildren;
+		}
+
+		
+		////////////////////////////////////////////////////////////////////////////////////////////////
+		/*--------------------------------------------------------------------------------------------*/
+		private void FillChildItemsList(Transform pParentTx) {
+			foreach ( Transform childTx in pParentTx ) {
+				HoverItemData item = childTx.GetComponent<HoverItemData>();
+
+				if ( item != null ) {
+					ChildItems.Add(item);
+				}
+
+				if ( !FindOnlyImmediateChildren ) {
+					FillChildItemsList(childTx);
+				}
+			}
+		}
+
+	}
+
+}
