426e0f31970c46da717918684c438a4217136f04
Zach Kinstner <zach@aestheticinteractive.com>
2016-06-30 22:13:15 -0400

    Separated "arc" parts of the "button fill" into an updater script. Updated the prefab accordingly.

--- Unity/Assets/Hover/Scripts/Renderers/Elements/HoverFillButton.cs
+++ Unity/Assets/Hover/Scripts/Renderers/Elements/HoverFillButton.cs
@@ -7,36 +7,23 @@ namespace Hover.Renderers.Elements {
 
 	/*================================================================================================*/
 	[RequireComponent(typeof(HoverIndicator))]
-	[RequireComponent(typeof(HoverShapeArc))]
-	public class HoverFillArcButton : HoverFill {
-
-		//TODO: refactor to just HoverFillButton, and handle shapes elsewhere?
-
-		public enum EdgePositionType {
-			Inner,
-			Outer
-		}
+	[RequireComponent(typeof(HoverShape))]
+	public class HoverFillButton : HoverFill {
 
 		[DisableWhenControlled(DisplayMessage=true)]
-		public HoverMeshArc Background;
+		public HoverMesh Background;
 
 		[DisableWhenControlled]
-		public HoverMeshArc Highlight;
+		public HoverMesh Highlight;
 
 		[DisableWhenControlled]
-		public HoverMeshArc Selection;
+		public HoverMesh Selection;
 
 		[DisableWhenControlled]
-		public HoverMeshArc Edge;
+		public HoverMesh Edge;
 		
 		[DisableWhenControlled]
 		public bool ShowEdge = true;
-
-		[DisableWhenControlled(RangeMin=0)]
-		public float EdgeThickness = 0.001f;
-
-		[DisableWhenControlled]
-		public EdgePositionType EdgePosition = EdgePositionType.Inner;
 
 
 		////////////////////////////////////////////////////////////////////////////////////////////////
@@ -69,49 +56,25 @@ namespace Hover.Renderers.Elements {
 		////////////////////////////////////////////////////////////////////////////////////////////////
 		/*--------------------------------------------------------------------------------------------*/
 		protected virtual void UpdateMeshes() {
-			HoverShapeArc shape = gameObject.GetComponent<HoverShapeArc>();
-
-			bool isOuterEdge = (EdgePosition == EdgePositionType.Outer);
-			float inset = EdgeThickness*Mathf.Sign(shape.OuterRadius-shape.InnerRadius);
-			float insetOuterRadius = shape.OuterRadius - (isOuterEdge ? inset : 0);
-			float insetInnerRadius = shape.InnerRadius + (isOuterEdge ? 0 : inset);
-			float edgeOuterRadius = (isOuterEdge ? shape.OuterRadius : insetInnerRadius);
-			float edgeInnerRadius = (isOuterEdge ? insetOuterRadius : shape.InnerRadius);
-
 			if ( Background != null ) {
-				UpdateMeshShape(Background, insetOuterRadius, insetInnerRadius);
 				UpdateMesh(Background, true);
 			}
 			
 			if ( Highlight != null ) {
-				UpdateMeshShape(Highlight, insetOuterRadius, insetInnerRadius);
 				UpdateMesh(Highlight, true);
 			}
 			
 			if ( Selection != null ) {
-				UpdateMeshShape(Selection, insetOuterRadius, insetInnerRadius);
 				UpdateMesh(Selection, true);
 			}
 			
 			if ( Edge != null ) {
-				UpdateMeshShape(Edge, edgeOuterRadius, edgeInnerRadius);
 				UpdateMesh(Edge, ShowEdge);
 			}
 		}
 		
 		/*--------------------------------------------------------------------------------------------*/
-		protected virtual void UpdateMeshShape(HoverMeshArc pMesh, float pOuterRad, float pInnerRad) {
-			HoverShapeArc meshShape = pMesh.GetComponent<HoverShapeArc>();
-
-			meshShape.Controllers.Set(HoverShapeArc.OuterRadiusName, this);
-			meshShape.Controllers.Set(HoverShapeArc.InnerRadiusName, this);
-			
-			meshShape.OuterRadius = pOuterRad;
-			meshShape.InnerRadius = pInnerRad;
-		}
-
-		/*--------------------------------------------------------------------------------------------*/
-		protected virtual void UpdateMesh(HoverMeshArc pMesh, bool pShowMesh) {
+		protected virtual void UpdateMesh(HoverMesh pMesh, bool pShowMesh) {
 			pMesh.Controllers.Set("GameObject.activeSelf", this);
 
 			//TODO: determine when mesh can be disabled (without needing a mesh update to occur first)
