51ca9714e99ac9929c7ee39d6c39b1992e861a11
Zach Kinstner <zach@aestheticinteractive.com>
2016-07-08 09:21:42 -0700

    Implemented new cursor renderer support. Replaced old cursor renderers with new ones in the HovercastTest scene.

---
+++ Unity/Assets/Hover/Scripts/Renderers/Elements/Cursors/HoverRendererCursor.cs
@@ -0,0 +1,66 @@
+using System;
+using Hover.Items;
+using Hover.Renderers.Contents;
+using Hover.Renderers.Elements.Contents;
+using Hover.Renderers.Elements.Shapes;
+using Hover.Utils;
+using UnityEngine;
+
+namespace Hover.Renderers.Elements.Cursors {
+
+	/*================================================================================================*/
+	public class HoverRendererCursor : HoverRenderer {
+
+		[DisableWhenControlled]
+		public HoverFillCursor Fill;
+
+
+		////////////////////////////////////////////////////////////////////////////////////////////////
+		/*--------------------------------------------------------------------------------------------*/
+		public override int GetChildFillCount() {
+			return 1;
+		}
+
+		/*--------------------------------------------------------------------------------------------*/
+		public override HoverFill GetChildFill(int pIndex) {
+			switch ( pIndex ) {
+				case 0: return Fill;
+			}
+
+			throw new ArgumentOutOfRangeException();
+		}
+		
+		/*--------------------------------------------------------------------------------------------*/
+		public override int GetChildRendererCount() {
+			return 0;
+		}
+
+		/*--------------------------------------------------------------------------------------------*/
+		public override HoverRenderer GetChildRenderer(int pIndex) {
+			throw new ArgumentOutOfRangeException();
+		}
+
+		/*--------------------------------------------------------------------------------------------*/
+		public override HoverCanvas GetCanvas() {
+			return null;
+		}
+
+		/*--------------------------------------------------------------------------------------------*/
+		public override HoverCanvasDataUpdater GetCanvasDataUpdater() {
+			return null;
+		}
+
+		/*--------------------------------------------------------------------------------------------*/
+		public override Vector3 GetNearestWorldPosition(Vector3 pFromWorldPosition) {
+			return Vector3.zero;
+		}
+
+		/*--------------------------------------------------------------------------------------------*/
+		public override Vector3 GetNearestWorldPosition(Ray pFromWorldRay, out RaycastResult pRaycast) {
+			pRaycast = new RaycastResult();
+			return Vector3.zero;
+		}
+
+	}
+
+}
