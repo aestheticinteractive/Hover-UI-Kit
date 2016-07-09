c57e7204b4e0735febe02fa778af7a8873967e1c
Zach Kinstner <zach@aestheticinteractive.com>
2016-07-08 20:15:09 -0400

    Implemented "LeapMotionOld" input module for pre-Orion support (useful for Mac users).

---
+++ Unity/Assets/Hover/InputModules/LeapMotionOld/Scripts/HoverInputLeapMotionOld.cs
@@ -0,0 +1,138 @@
+﻿#if !HOVER_IGNORE_LEAPOLD
+
+using System;
+using System.Collections.Generic;
+using Hover.Cursors;
+using Leap;
+using UnityEngine;
+
+namespace Hover.InputModules.LeapMotionOld {
+
+	/*================================================================================================*/
+	[ExecuteInEditMode]
+	public class HoverInputLeapMotionOld : MonoBehaviour {
+
+		private static readonly Quaternion RotationFix = Quaternion.Euler(90, 90, 90);
+
+		public HoverCursorDataProvider CursorDataProvider;
+		public HandController LeapControl;
+		public bool UseStabilizedPositions = false;
+		//TODO: Add "extend cursor beyond fingertip" setting
+
+
+		////////////////////////////////////////////////////////////////////////////////////////////////
+		/*--------------------------------------------------------------------------------------------*/
+		public void Awake() {
+			if ( CursorDataProvider == null ) {
+				CursorDataProvider = FindObjectOfType<HoverCursorDataProvider>();
+			}
+
+			if ( LeapControl == null ) {
+				LeapControl = FindObjectOfType<HandController>();
+			}
+		}
+
+		/*--------------------------------------------------------------------------------------------*/
+		public void Update() {
+			if ( !Application.isPlaying ) {
+				return;
+			}
+
+			if ( CursorDataProvider == null || LeapControl == null ) {
+				Debug.LogError("References to "+typeof(HoverCursorDataProvider).Name+" and "+
+					typeof(HandController).Name+" must be set.", this);
+				return;
+			}
+
+			CursorDataProvider.MarkAllCursorsUnused();
+			UpdateCursorsWithHands(LeapControl.GetFrame().Hands);
+			CursorDataProvider.ActivateAllCursorsBasedOnUsage();
+		}
+
+
+		////////////////////////////////////////////////////////////////////////////////////////////////
+		/*--------------------------------------------------------------------------------------------*/
+		private void UpdateCursorsWithHands(HandList pLeapHands) {
+			for ( int i = 0 ; i < pLeapHands.Count ; i++ ) {
+				UpdateCursorsWithHand(pLeapHands[i]);
+			}
+		}
+
+		/*--------------------------------------------------------------------------------------------*/
+		private void UpdateCursorsWithHand(Hand pLeapHand) {
+			UpdateCursorsWithPalm(pLeapHand);
+
+			for ( int i = 0 ; i < pLeapHand.Fingers.Count ; i++ ) {
+				UpdateCursorsWithFinger(pLeapHand, pLeapHand.Fingers[i]);
+			}
+		}
+
+		/*--------------------------------------------------------------------------------------------*/
+		private void UpdateCursorsWithPalm(Hand pLeapHand) {
+			CursorType cursorType = (pLeapHand.IsLeft ? CursorType.LeftPalm : CursorType.RightPalm);
+
+			if ( !CursorDataProvider.HasCursorData(cursorType) ) {
+				return;
+			}
+
+			Transform leapTx = LeapControl.transform;
+			Vector palmPos = (UseStabilizedPositions ? 
+				pLeapHand.StabilizedPalmPosition : pLeapHand.PalmPosition);
+
+			IHoverCursorDataForInput data = CursorDataProvider.GetCursorDataForInput(cursorType);
+			data.SetWorldPosition(leapTx.TransformPoint(palmPos.ToUnityScaled()));
+			data.SetWorldRotation(leapTx.rotation*pLeapHand.Basis.Rotation()*RotationFix);
+			data.SetSize(pLeapHand.PalmWidth*UnityVectorExtension.INPUT_SCALE);
+			data.SetTriggerStrength(pLeapHand.GrabStrength);
+			data.SetUsedByInput(true);
+		}
+
+		/*--------------------------------------------------------------------------------------------*/
+		private void UpdateCursorsWithFinger(Hand pLeapHand, Finger pLeapFinger) {
+			CursorType cursorType = GetFingerCursorType(pLeapHand.IsLeft, pLeapFinger.Type);
+
+			if ( !CursorDataProvider.HasCursorData(cursorType) ) {
+				return;
+			}
+
+			Transform leapTx = LeapControl.transform;
+			Vector tipPos = (UseStabilizedPositions ? 
+				pLeapFinger.StabilizedTipPosition: pLeapFinger.TipPosition);
+			Bone distalBone = pLeapFinger.Bone(Bone.BoneType.TYPE_DISTAL);
+
+			IHoverCursorDataForInput data = CursorDataProvider.GetCursorDataForInput(cursorType);
+			data.SetWorldPosition(leapTx.TransformPoint(tipPos.ToUnityScaled()));
+			data.SetWorldRotation(leapTx.rotation*distalBone.Basis.Rotation()*RotationFix);
+			data.SetSize(pLeapFinger.Width*UnityVectorExtension.INPUT_SCALE);
+			data.SetUsedByInput(true);
+		}
+
+
+		////////////////////////////////////////////////////////////////////////////////////////////////
+		/*--------------------------------------------------------------------------------------------*/
+		private CursorType GetFingerCursorType(bool pIsLeft, Finger.FingerType pLeapFingerType) {
+			switch ( pLeapFingerType ) {
+				case Finger.FingerType.TYPE_THUMB:
+					return (pIsLeft ? CursorType.LeftThumb : CursorType.RightThumb);
+					
+				case Finger.FingerType.TYPE_INDEX:
+					return (pIsLeft ? CursorType.LeftIndex : CursorType.RightIndex);
+					
+				case Finger.FingerType.TYPE_MIDDLE:
+					return (pIsLeft ? CursorType.LeftMiddle : CursorType.RightMiddle);
+					
+				case Finger.FingerType.TYPE_RING:
+					return (pIsLeft ? CursorType.LeftRing : CursorType.RightRing);
+					
+				case Finger.FingerType.TYPE_PINKY:
+					return (pIsLeft ? CursorType.LeftPinky : CursorType.RightPinky);
+			}
+			
+			throw new Exception("Unhandled cursor combination: "+pIsLeft+" / "+pLeapFingerType);
+		}
+
+	}
+
+}
+
+#endif //!HOVER_IGNORE_LEAPOLD
--- Unity/Assets/README.md
+++
@@ -1,8 +0,0 @@
-### Important Note
-
-The **Hover VR** code repository does _not_ include the Leap Motion assets.
-
-Before working with this project:
-
-1. Download the [Leap Motion 3.0.0 assets](https://developer.leapmotion.com/downloads/unity).
-2. Install those assets into this Unity project.
