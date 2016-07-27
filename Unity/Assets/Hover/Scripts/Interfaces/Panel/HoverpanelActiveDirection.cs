9651066b2c4a1361cd4083a6d99fa694ab48c1c3
Zach Kinstner <zach@aestheticinteractive.com>
2016-07-27 09:12:28 -0400

    Implemented "active direction" for the panel interface, allowing panels to be hidden automatically when not facing the camera (useful for the "rotate" transitions).

---
+++ Unity/Assets/Hover/Scripts/Interfaces/Panel/HoverpanelActiveDirection.cs
@@ -0,0 +1,69 @@
+﻿using Hover.Layouts.Rect;
+using Hover.Utils;
+using UnityEngine;
+
+namespace Hover.Interfaces.Panel {
+
+	/*================================================================================================*/
+	[ExecuteInEditMode]
+	[RequireComponent(typeof(TreeUpdater))]
+	[RequireComponent(typeof(HoverpanelInterface))]
+	[RequireComponent(typeof(HoverpanelRowTransitioner))]
+	public class HoverpanelActiveDirection : MonoBehaviour, ITreeUpdateable, ISettingsController {
+
+		public Transform ActiveWhenFacing;
+		public bool OnlyDuringTransitions = true;
+
+
+		////////////////////////////////////////////////////////////////////////////////////////////////
+		/*--------------------------------------------------------------------------------------------*/
+		public void Awake() {
+			if ( ActiveWhenFacing == null ) {
+				ActiveWhenFacing = Camera.main.transform;
+			}
+		}
+		
+		/*--------------------------------------------------------------------------------------------*/
+		public void Start() {
+			//do nothing...
+		}
+
+		/*--------------------------------------------------------------------------------------------*/
+		public void TreeUpdate() {
+			HoverpanelInterface panel = GetComponent<HoverpanelInterface>();
+			HoverpanelRowTransitioner trans = GetComponent<HoverpanelRowTransitioner>();
+
+			if ( OnlyDuringTransitions && !trans.IsTransitionActive ) {
+				return;
+			}
+
+			UpdateRow(panel.ActiveRow);
+			UpdateRow(panel.PreviousRow);
+		}
+
+
+		////////////////////////////////////////////////////////////////////////////////////////////////
+		/*--------------------------------------------------------------------------------------------*/
+		private void UpdateRow(HoverLayoutRectRow pRow) {
+			if ( pRow == null || !pRow.gameObject.activeSelf ) {
+				return;
+			}
+
+			Vector3 panelWorldNorm = pRow.transform.TransformDirection(Vector3.back);
+			Vector3 panelToTxWorldDir = (ActiveWhenFacing.position-pRow.transform.position).normalized;
+			float dotBetweenDirs = Vector3.Dot(panelWorldNorm, panelToTxWorldDir);
+
+			if ( dotBetweenDirs > 0 ) {
+				return;
+			}
+
+			pRow.gameObject.SetActive(false);
+
+			//Vector3 panelPos = pRow.transform.position;
+			//Debug.DrawLine(panelPos, panelPos+panelWorldNorm, Color.red);
+			//Debug.DrawLine(panelPos, panelPos+panelToTxWorldDir, Color.cyan);
+		}
+
+	}
+
+}
