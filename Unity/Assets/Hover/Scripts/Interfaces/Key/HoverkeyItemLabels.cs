97de9f0768b6d7aa2e6ec2b4f3dfecba4dacab27
Zach Kinstner <zach@aestheticinteractive.com>
2016-07-30 14:06:22 -0400

    Added initial "key labels" script. Added labels to the number-row of the keyboard layout.

---
+++ Unity/Assets/Hover/Scripts/Interfaces/Key/HoverkeyItemLabels.cs
@@ -0,0 +1,17 @@
+﻿using Hover.Layouts.Rect;
+using Hover.Utils;
+using UnityEngine;
+
+namespace Hover.Interfaces.Key {
+
+	/*================================================================================================*/
+	public class HoverkeyItemLabels : MonoBehaviour {
+
+		public KeyCode DefaultKey;
+		public string DefaultLabel;
+		public bool HasShiftLabel;
+		public string ShiftLabel;
+
+	}
+
+}
