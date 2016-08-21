97cb6d2a49903b283255cfae558169a274d0dec9
Zach Kinstner <zach@aestheticinteractive.com>
2016-08-21 11:52:10 -0400

    Continued work on the Hoverkey builder.

--- Unity/Assets/Hover/InterfaceModules/Key/Scripts/HoverkeyBuilder.cs
+++ Unity/Assets/Hover/InterfaceModules/Key/Scripts/HoverkeyBuilder.cs
@@ -6,6 +6,7 @@ using Hover.Core.Renderers.Shapes.Arc;
 using Hover.Core.Renderers.Shapes.Rect;
 using Hover.Core.Utils;
 using UnityEngine;
+using Hover.Core.Renderers.Contents;
 
 namespace Hover.InterfaceModules.Key {
 
@@ -23,12 +24,12 @@ namespace Hover.InterfaceModules.Key {
 		public void Awake() {
 			if ( ButtonRendererPrefab == null ) {
 				ButtonRendererPrefab = Resources.Load<GameObject>(
-					"Prefabs/HoverAlphaButtonArcRenderer-Default");
+					"Prefabs/HoverAlphaButtonRectRenderer-Default");
 			}
 
 			if ( SliderRendererPrefab == null ) {
 				SliderRendererPrefab = Resources.Load<GameObject>(
-					"Prefabs/HoverAlphaSliderArcRenderer-Default");
+					"Prefabs/HoverAlphaSliderRectRenderer-Default");
 			}
 		}
 		
@@ -51,47 +52,76 @@ namespace Hover.InterfaceModules.Key {
 			gameObject.AddComponent<HoverkeyManager>();
 
 			BuildMain();
+			BuildFunctions();
+
+			TreeUpdater treeUp = gameObject.AddComponent<TreeUpdater>();
+			treeUp.Update(); //force entire interface update 
 		}
 
 
 		////////////////////////////////////////////////////////////////////////////////////////////////
 		/*--------------------------------------------------------------------------------------------*/
 		private void BuildMain() {
-			var fullGo = new GameObject("Main");
-			fullGo.transform.SetParent(gameObject.transform, false);
-			fullGo.transform.localPosition = new Vector3(0.01125f, 0, 0);
+			var mainGo = new GameObject("Main");
+			mainGo.transform.SetParent(gameObject.transform, false);
+			mainGo.transform.localPosition = new Vector3(0.01125f, 0, 0);
 
-			fullGo.AddComponent<TreeUpdater>();
+			mainGo.AddComponent<TreeUpdater>();
 
-			HoverLayoutRectRow rectRow = fullGo.AddComponent<HoverLayoutRectRow>();
+			HoverLayoutRectRow rectRow = mainGo.AddComponent<HoverLayoutRectRow>();
 			rectRow.Arrangement = HoverLayoutRectRow.ArrangementType.TopToBottom;
 			rectRow.SizeX = 0.4425f;
 			rectRow.SizeY = 0.15f;
 
-			BuildRow("Row0", fullGo.transform, HoverkeyBuilderData.MainRow0);
-			BuildRow("Row1", fullGo.transform, HoverkeyBuilderData.MainRow1);
-			BuildRow("Row2", fullGo.transform, HoverkeyBuilderData.MainRow2);
-			BuildRow("Row3", fullGo.transform, HoverkeyBuilderData.MainRow3);
-			BuildRow("Row4", fullGo.transform, HoverkeyBuilderData.MainRow4);
+			BuildRow("Row0", mainGo.transform, HoverkeyBuilderData.MainRow0);
+			BuildRow("Row1", mainGo.transform, HoverkeyBuilderData.MainRow1);
+			BuildRow("Row2", mainGo.transform, HoverkeyBuilderData.MainRow2);
+			BuildRow("Row3", mainGo.transform, HoverkeyBuilderData.MainRow3);
+			BuildRow("Row4", mainGo.transform, HoverkeyBuilderData.MainRow4);
+		}
+
+		/*--------------------------------------------------------------------------------------------*/
+		private void BuildFunctions() {
+			var funcGo = new GameObject("Functions");
+			funcGo.transform.SetParent(gameObject.transform, false);
+			funcGo.transform.localPosition = new Vector3(0.01125f, 0.105f, 0);
+
+			funcGo.AddComponent<TreeUpdater>();
+
+			HoverLayoutRectRow rectRow = funcGo.AddComponent<HoverLayoutRectRow>();
+			rectRow.Arrangement = HoverLayoutRectRow.ArrangementType.LeftToRight;
+			rectRow.SizeX = 0.4425f;
+			rectRow.SizeY = 0.03f;
+			rectRow.InnerPadding = 0.0175f;
+
+			GameObject esctGo = BuildRowItem(funcGo.transform, HoverkeyBuilderData.FuncRow[0]);
+			HoverLayoutRectRelativeSizer escSizer = esctGo.AddComponent<HoverLayoutRectRelativeSizer>();
+
+			escSizer.RelativeSizeX = 0.25f;
+
+			BuildRow("Row0", funcGo.transform, HoverkeyBuilderData.FuncRow, 1, 4);
+			BuildRow("Row1", funcGo.transform, HoverkeyBuilderData.FuncRow, 5, 4);
+			BuildRow("Row2", funcGo.transform, HoverkeyBuilderData.FuncRow, 9, 4);
 		}
 
 
 		////////////////////////////////////////////////////////////////////////////////////////////////
 		/*--------------------------------------------------------------------------------------------*/
-		private void BuildRow(string pName, Transform pParentTx, HoverkeyBuilderKeyInfo[] pKeys) {
+		private void BuildRow(string pName, Transform pParentTx, HoverkeyBuilderKeyInfo[] pKeys,
+														int pStartIndex=0, int pCount=int.MaxValue) {
 			var rowGo = new GameObject(pName);
 			rowGo.transform.SetParent(pParentTx, false);
 
 			rowGo.AddComponent<TreeUpdater>();
 			rowGo.AddComponent<HoverLayoutRectRow>();
 
-			foreach ( HoverkeyBuilderKeyInfo keyInfo in pKeys ) {
-				BuildRowItem(rowGo.transform, keyInfo);
+			for ( int i = pStartIndex ; i < pKeys.Length && i < pStartIndex+pCount ; i++ ) {
+				BuildRowItem(rowGo.transform, pKeys[i]);
 			}
 		}
 
 		/*--------------------------------------------------------------------------------------------*/
-		private void BuildRowItem(Transform pRowTx, HoverkeyBuilderKeyInfo pKeyInfo) {
+		private GameObject BuildRowItem(Transform pRowTx, HoverkeyBuilderKeyInfo pKeyInfo) {
 			var itemGo = new GameObject(pKeyInfo.DefaultKey+"");
 			itemGo.transform.SetParent(pRowTx, false);
 
@@ -100,6 +130,8 @@ namespace Hover.InterfaceModules.Key {
 			build.ButtonRendererPrefab = ButtonRendererPrefab;
 			build.SliderRendererPrefab = SliderRendererPrefab;
 			build.PerformBuild();
+
+			////
 
 			HoverItemData data = itemGo.GetComponent<HoverItemData>();
 			data.Id = pKeyInfo.ID;
@@ -119,18 +151,50 @@ namespace Hover.InterfaceModules.Key {
 					itemGo.AddComponent<HoverLayoutRectRelativeSizer>();
 				sizer.RelativeSizeX = pKeyInfo.RelativeSizeX;
 			}
-		}
 
-		/*--------------------------------------------------------------------------------------------*/
-		private void BuildRowSliderItem(GameObject pRowGo, string pId, string pLabel) {
-			GameObject sliderItemGo =
-				BuildRowItem(pRowGo, "Row2-ItemA", "BA", HoverItem.HoverItemType.Slider);
+			HoverCanvas can = itemGo.GetComponentInChildren<HoverCanvas>();
+			can.Alignment = HoverCanvas.CanvasAlignmentType.Center;
+			can.PaddingX = 0.001f;
 
+			////
 
-			HoverItemDataSlider data = sliderItemGo.GetComponent<HoverItemDataSlider>();
-			data.Value = 0.825f;
-			data.Ticks = 5;
-			data.AllowJump = true;
+			HoverkeyManager inter = gameObject.GetComponent<HoverkeyManager>();
+			HoverItemDataSelector selData = (data as HoverItemDataSelector);
+			HoverItemDataSticky stickyData = (data as HoverItemDataSticky);
+			HoverItemDataCheckbox checkboxData = (data as HoverItemDataCheckbox);
+
+			if ( selData != null ) {
+#if UNITY_EDITOR
+				UnityEditor.Events.UnityEventTools.AddPersistentListener(
+					selData.OnSelectedEvent, inter.HandleItemSelected);
+#else
+				selData.OnSelectedEvent.AddListener(inter.HandleItemSelected);
+#endif
+			}
+			else if ( stickyData != null ) {
+#if UNITY_EDITOR
+				UnityEditor.Events.UnityEventTools.AddPersistentListener(
+					stickyData.OnSelectedEvent, inter.HandleItemSelected);
+				UnityEditor.Events.UnityEventTools.AddPersistentListener(
+					stickyData.OnDeselectedEvent, inter.HandleItemDeselected);
+#else
+				stickyData.OnSelectedEvent.AddListener(inter.HandleItemSelected);
+				stickyData.OnDeselectedEvent.AddListener(inter.HandleItemDeselected);
+#endif
+			}
+			else if ( checkboxData != null ) {
+#if UNITY_EDITOR
+				UnityEditor.Events.UnityEventTools.AddPersistentListener(
+					checkboxData.OnSelectedEvent, inter.HandleItemSelected);
+				UnityEditor.Events.UnityEventTools.AddPersistentListener(
+					checkboxData.OnValueChangedEvent, inter.HandleItemValueChanged);
+#else
+				checkboxData.OnSelectedEvent.AddListener(inter.HandleItemSelected);
+				checkboxData.OnValueChangedEvent.AddListener(inter.HandleItemValueChanged);
+#endif
+			}
+
+			return itemGo;
 		}
 
 	}
