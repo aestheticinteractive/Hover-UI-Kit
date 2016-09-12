27b9fb18750e4bea70fc10c0f7fe14f98807a564
Zach Kinstner <zach@aestheticinteractive.com>
2016-09-12 09:08:59 -0400

    Updated docs with new HoverInteractionSettings property names.

--- html/class_hover_1_1_core_1_1_cursors_1_1_hover_interaction_settings.js
+++ html/class_hover_1_1_core_1_1_cursors_1_1_hover_interaction_settings.js
@@ -3,8 +3,8 @@ var class_hover_1_1_core_1_1_cursors_1_1_hover_interaction_settings =
     [ "Update", "class_hover_1_1_core_1_1_cursors_1_1_hover_interaction_settings.html#a6183782c06644ab7892a49c736ed42b8", null ],
     [ "HighlightDistanceMax", "class_hover_1_1_core_1_1_cursors_1_1_hover_interaction_settings.html#a053b6ec99d7b7c74e2f89b7d7421b2ec", null ],
     [ "HighlightDistanceMin", "class_hover_1_1_core_1_1_cursors_1_1_hover_interaction_settings.html#a300ca4c9d8ae8e00b02f26d0f486aca9", null ],
-    [ "MotionlessDistanceThreshold", "class_hover_1_1_core_1_1_cursors_1_1_hover_interaction_settings.html#aa41134f2cd39cb1fff81b11e7bfe53f7", null ],
-    [ "MotionlessMilliseconds", "class_hover_1_1_core_1_1_cursors_1_1_hover_interaction_settings.html#a85c36f66091071a90d8749ece1bc42ae", null ],
+    [ "IdleDistanceThreshold", "class_hover_1_1_core_1_1_cursors_1_1_hover_interaction_settings.html#ad97de43657134bbdd50005338de321e0", null ],
+    [ "IdleMilliseconds", "class_hover_1_1_core_1_1_cursors_1_1_hover_interaction_settings.html#a152123cf3bce00481309c496c24895fb", null ],
     [ "SelectionMilliseconds", "class_hover_1_1_core_1_1_cursors_1_1_hover_interaction_settings.html#ab2bb77eaeb16c3050f28325f387f28ab", null ],
     [ "StickyReleaseDistance", "class_hover_1_1_core_1_1_cursors_1_1_hover_interaction_settings.html#aa9f4aff48fc8690699ac40160fe41265", null ]
 ];
