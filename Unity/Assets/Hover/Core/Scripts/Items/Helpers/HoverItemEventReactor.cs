bc6d871aa301a0fbab02ad0599330e5abdecdc42
Zach Kinstner <zach@aestheticinteractive.com>
2016-10-04 08:46:35 -0400

    Added an initial "event reactor" component (needs new name?) to make it easier to enable/disable items based on another item's state.

---
+++ Unity/Assets/Hover/Core/Scripts/Items/Helpers/HoverItemEventReactor.cs
@@ -0,0 +1,24 @@
+﻿using Hover.Core.Items.Types;
+using UnityEngine;
+
+namespace Hover.Core.Items.Helpers {
+
+	/*================================================================================================*/
+	[RequireComponent(typeof(HoverItemData))]
+	public class HoverItemEventReactor : MonoBehaviour {
+
+
+		////////////////////////////////////////////////////////////////////////////////////////////////
+		/*--------------------------------------------------------------------------------------------*/
+		public void EnableWithBoolValue(HoverItemDataSelectableBool pItemData) {
+			GetComponent<HoverItemData>().IsEnabled = pItemData.Value;
+		}
+
+		/*--------------------------------------------------------------------------------------------*/
+		public void DisableWithBoolValue(HoverItemDataSelectableBool pItemData) {
+			GetComponent<HoverItemData>().IsEnabled = !pItemData.Value;
+		}
+
+	}
+
+}
