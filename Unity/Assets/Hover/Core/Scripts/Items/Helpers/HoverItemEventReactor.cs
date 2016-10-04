64344e62e9f000afe9604223b373d1a4cce7a7bc
Zach Kinstner <zach@aestheticinteractive.com>
2016-10-04 10:52:30 -0400

    Added "nested" rows, items, and events for the "CastCubes" example scene.

--- Unity/Assets/Hover/Core/Scripts/Items/Helpers/HoverItemEventReactor.cs
+++ Unity/Assets/Hover/Core/Scripts/Items/Helpers/HoverItemEventReactor.cs
@@ -19,6 +19,16 @@ namespace Hover.Core.Items.Helpers {
 			GetComponent<HoverItemData>().IsEnabled = !pItemData.Value;
 		}
 
+		/*--------------------------------------------------------------------------------------------*/
+		public void ShowWithBoolValue(IItemDataSelectable<bool> pItemData) {
+			GetComponent<HoverItemData>().gameObject.SetActive(pItemData.Value);
+		}
+
+		/*--------------------------------------------------------------------------------------------*/
+		public void HideWithBoolValue(IItemDataSelectable<bool> pItemData) {
+			GetComponent<HoverItemData>().gameObject.SetActive(!pItemData.Value);
+		}
+
 	}
 
 }
