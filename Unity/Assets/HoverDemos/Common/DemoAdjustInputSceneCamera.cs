35925a470021d1227370904a6df19e329206bac7
Zach Kinstner <zach@aestheticinteractive.com>
2016-10-13 10:12:50 -0400

    Implemented an input-scene adjuster shared across all demo scenes. Updated demo scenes to use the new adjuster.

--- Unity/Assets/HoverDemos/Common/DemoAdjustInputSceneCamera.cs
+++ Unity/Assets/HoverDemos/Common/DemoAdjustInputSceneCamera.cs
@@ -2,20 +2,32 @@
 using UnityEngine;
 using UnityStandardAssets.ImageEffects;
 
-namespace HoverDemos.CastCubes.Inputs {
+namespace HoverDemos.Common {
 
 	/*================================================================================================*/
-	public abstract class DemoAdjustInputSceneBase : HoverSceneAdjust {
+	public class DemoAdjustInputSceneCamera : HoverSceneAdjust {
+
+		public string CameraName;
+		public string MainSceneCameraName = "MainCamera";
+		public bool UseBloom = true;
 
 
 		////////////////////////////////////////////////////////////////////////////////////////////////
 		/*--------------------------------------------------------------------------------------------*/
-		protected void ConfigureCamera(string pCameraName) {
-			Camera cam = UnityUtil.FindComponentAll<Camera>(pCameraName);
+		protected override bool IsReadyToAdjust() {
+			return (GameObject.Find(CameraName) != null);
+		}
+
+		/*--------------------------------------------------------------------------------------------*/
+		protected override void PerformAdjustments() {
+			Camera mainSceneCam = UnityUtil.FindComponentAll<Camera>(MainSceneCameraName);
+			mainSceneCam.gameObject.SetActive(false);
+
+			Camera cam = UnityUtil.FindComponentAll<Camera>(CameraName);
 			cam.clearFlags = CameraClearFlags.Color;
 			cam.backgroundColor = Color.black;
 
-			if ( cam.GetComponent<BloomOptimized>() == null ) {
+			if ( UseBloom && cam.GetComponent<BloomOptimized>() == null ) {
 				BloomOptimized bloom = cam.gameObject.AddComponent<BloomOptimized>();
 				bloom.threshold = 0.35f;
 				bloom.intensity = 1;
