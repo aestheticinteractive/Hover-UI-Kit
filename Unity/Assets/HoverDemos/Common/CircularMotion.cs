6b009ecc4e3712f83f9d0cc74cdcaebbeca39a0c
Zach Kinstner <zach@aestheticinteractive.com>
2016-11-11 12:57:35 -0500

    Created demo scene for the new animated "H" logo.

---
+++ Unity/Assets/HoverDemos/Common/CircularMotion.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+namespace HoverDemos.Common {
+
+	/*================================================================================================*/
+	[ExecuteInEditMode]
+	public class CircularMotion : MonoBehaviour {
+
+		public Vector3 CenterWorldPos = Vector3.zero;
+		public Vector3 RotationWorldAxis = Vector3.forward;
+		public Vector3 RotationWorldUp = Vector3.up;
+
+		[Range(0, 0.2f)]
+		public float Radius = 0.06f;
+
+		[Range(0, 360)]
+		public float Speed = 20;
+
+
+		////////////////////////////////////////////////////////////////////////////////////////////////
+		/*--------------------------------------------------------------------------------------------*/
+		public void Update() {
+			if ( !Application.isPlaying ) {
+				transform.position = CenterWorldPos;
+				return;
+			}
+
+			Quaternion rot = Quaternion.AngleAxis(Time.time*Speed, RotationWorldAxis);
+			transform.position = CenterWorldPos + rot*(RotationWorldUp*Radius);
+		}
+
+	}
+
+}
---
+++ Unity/Assets/HoverDemos/Common/LookAt.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace HoverDemos.Common {
+
+	/*================================================================================================*/
+	[ExecuteInEditMode]
+	public class LookAt : MonoBehaviour {
+
+		public Transform Target;
+
+
+		////////////////////////////////////////////////////////////////////////////////////////////////
+		/*--------------------------------------------------------------------------------------------*/
+		public void Update() {
+			transform.LookAt(Target);
+			//transform.rotation *= Quaternion.FromToRotation(Vector3.forward, Vector3.back);
+		}
+
+	}
+
+}
