e44249024f8fe0febed51adb9aaa3d988717437c
Zach Kinstner <zach@aestheticinteractive.com>
2016-12-14 16:36:05 -0500

    Updated demo scenes with support for the Oculus Touch input module.

---
+++ Unity/Assets/HoverDemos/CastCubes/Scripts/Inputs/DemoAdjustMainSceneForOculusTouch.cs
@@ -0,0 +1,35 @@
+﻿using Hover.Core.Cursors;
+using Hover.Core.Items.Types;
+using Hover.Core.Utils;
+using Hover.InterfaceModules.Cast;
+using UnityEngine;
+
+namespace HoverDemos.CastCubes.Inputs {
+
+	/*================================================================================================*/
+	public class DemoAdjustMainSceneForOculusTouch : HoverSceneAdjust {
+
+
+		////////////////////////////////////////////////////////////////////////////////////////////////
+		/*--------------------------------------------------------------------------------------------*/
+		protected override bool IsReadyToAdjust() {
+			return (FindObjectOfType<HovercastInterface>() != null);
+		}
+
+		/*--------------------------------------------------------------------------------------------*/
+		protected override void PerformAdjustments() {
+			HovercastInterface cast = FindObjectOfType<HovercastInterface>();
+			DemoHovercastCustomizer castCustom = cast.GetComponent<DemoHovercastCustomizer>();
+
+			Deactivate(UnityUtil.FindComponentAll<Camera>("MainCamera").gameObject);
+			Deactivate(UnityUtil.FindComponentAll<HoverItemDataSelector>("Re-orient"));
+
+			castCustom.StandardFollowCursor = CursorType.LeftPinky;
+			castCustom.SwitchedFollowCursor = CursorType.RightPinky;
+			//castCustom.StandardInteractionCursor = CursorType.RightMiddle;
+			//castCustom.SwitchedInteractionCursor = CursorType.LeftMiddle;
+		}
+
+	}
+
+}
---
+++ Unity/Assets/HoverDemos/Common/DemoAdjustCursorsForHovercastOculusTouch.cs
@@ -0,0 +1,55 @@
+﻿using Hover.Core.Cursors;
+using Hover.Core.Utils;
+using Hover.InterfaceModules.Cast;
+using UnityEngine;
+
+namespace HoverDemos.Common {
+
+	/*================================================================================================*/
+	public class DemoAdjustCursorsForHovercastOculusTouch : HoverSceneAdjust {
+
+
+		////////////////////////////////////////////////////////////////////////////////////////////////
+		/*--------------------------------------------------------------------------------------------*/
+		protected override bool IsReadyToAdjust() {
+			return (FindObjectOfType<HovercastInterface>() != null);
+		}
+
+		/*--------------------------------------------------------------------------------------------*/
+		protected override void PerformAdjustments() {
+			HoverCursorDataProvider cursProv = FindObjectOfType<HoverCursorDataProvider>();
+			ICursorDataForInput pinL = cursProv.GetCursorDataForInput(CursorType.LeftPinky);
+			ICursorDataForInput indR = cursProv.GetCursorDataForInput(CursorType.RightIndex);
+			ICursorDataForInput look = cursProv.GetCursorDataForInput(CursorType.Look);
+			HovercastInterface cast = FindObjectOfType<HovercastInterface>();
+			HoverCursorFollower castFollow = cast.GetComponent<HoverCursorFollower>();
+			HoverCursorRendererUpdater[] cursorRendUps =
+				Resources.FindObjectsOfTypeAll<HoverCursorRendererUpdater>();
+
+			foreach ( HoverCursorRendererUpdater rendUp in cursorRendUps ) {
+				CursorType ct = rendUp.GetComponent<HoverCursorFollower>().CursorType;
+				rendUp.gameObject.SetActive(ct != CursorType.LeftPinky && ct != CursorType.RightPinky);
+			}
+
+			foreach ( ICursorData cursorData in cursProv.Cursors ) {
+				ICursorDataForInput cursorDataInp = cursProv.GetCursorDataForInput(cursorData.Type);
+				cursorDataInp.SetCapability(CursorCapabilityType.None);
+			}
+
+			pinL.SetCapability(CursorCapabilityType.TransformOnly);
+			indR.SetCapability(CursorCapabilityType.Full);
+			look.SetCapability(CursorCapabilityType.TransformOnly);
+
+			cast.transform.GetChild(0).localPosition = Vector3.zero; //moves "TransformAdjuster"
+			castFollow.CursorType = pinL.Type;
+
+			//for non-playing editor...
+
+			pinL.SetWorldRotation(Quaternion.Euler(0, 160, 80)); //face the camera (for editor)
+			castFollow.Update(); //moves interface to the correct cursor transform
+			cast.GetComponent<TreeUpdater>().Update(); //forces entire interface to update
+		}
+
+	}
+
+}
