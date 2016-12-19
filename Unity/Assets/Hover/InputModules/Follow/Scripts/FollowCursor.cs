8f366ebb1ac8d6815dcd22172044ba3b6adf53d8
Zach Kinstner <zach@aestheticinteractive.com>
2016-12-19 09:57:38 -0500

    Added "follow" input module, which simply uses a transform (for example, from the VR headset) to control a cursor.

---
+++ Unity/Assets/Hover/InputModules/Follow/Scripts/FollowCursor.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Hover.Core.Cursors;
+using UnityEngine;
+
+namespace Hover.InputModules.Follow {
+
+	/*================================================================================================*/
+	[Serializable]
+	public class FollowCursor {
+
+		public CursorType Type { get; private set; }
+
+		public Transform FollowTransform;
+
+
+		////////////////////////////////////////////////////////////////////////////////////////////////
+		/*--------------------------------------------------------------------------------------------*/
+		public FollowCursor(CursorType pType) {
+			Type = pType;
+		}
+
+
+		////////////////////////////////////////////////////////////////////////////////////////////////
+		/*--------------------------------------------------------------------------------------------*/
+		public void UpdateData(HoverCursorDataProvider pCursorDataProv) {
+			if ( !pCursorDataProv.HasCursorData(Type) ) {
+				return;
+			}
+
+			ICursorDataForInput data = pCursorDataProv.GetCursorDataForInput(Type);
+
+			if ( data == null ) {
+				return;
+			}
+
+			if ( FollowTransform == null ) {
+				data.SetUsedByInput(false);
+				return;
+			}
+
+			data.SetUsedByInput(FollowTransform.gameObject.activeInHierarchy);
+			data.SetWorldPosition(FollowTransform.position);
+			data.SetWorldRotation(FollowTransform.rotation);
+		}
+
+	}
+
+}
---
+++ Unity/Assets/Hover/InputModules/Follow/Scripts/HoverInputFollow.cs
@@ -0,0 +1,79 @@
+﻿using Hover.Core.Cursors;
+using Hover.Core.Utils;
+using UnityEngine;
+
+namespace Hover.InputModules.Follow {
+
+	/*================================================================================================*/
+	[ExecuteInEditMode]
+	public class HoverInputFollow : MonoBehaviour {
+
+		public HoverCursorDataProvider CursorDataProvider;
+
+		[Space(12)]
+
+		public FollowCursor Look = new FollowCursor(CursorType.Look);
+
+		[Space(12)]
+
+		public FollowCursor LeftPalm = new FollowCursor(CursorType.LeftPalm);
+		public FollowCursor LeftThumb = new FollowCursor(CursorType.LeftThumb);
+		public FollowCursor LeftIndex = new FollowCursor(CursorType.LeftIndex);
+		public FollowCursor LeftMiddle = new FollowCursor(CursorType.LeftMiddle);
+		public FollowCursor LeftRing = new FollowCursor(CursorType.LeftRing);
+		public FollowCursor LeftPinky = new FollowCursor(CursorType.LeftPinky);
+
+		[Space(12)]
+
+		public FollowCursor RightPalm = new FollowCursor(CursorType.RightPalm);
+		public FollowCursor RightThumb = new FollowCursor(CursorType.RightThumb);
+		public FollowCursor RightIndex = new FollowCursor(CursorType.RightIndex);
+		public FollowCursor RightMiddle = new FollowCursor(CursorType.RightMiddle);
+		public FollowCursor RightRing = new FollowCursor(CursorType.RightRing);
+		public FollowCursor RightPinky = new FollowCursor(CursorType.RightPinky);
+
+
+		////////////////////////////////////////////////////////////////////////////////////////////////
+		/*--------------------------------------------------------------------------------------------*/
+		public void Awake() {
+			InputModuleUtil.FindCursorReference(this, ref CursorDataProvider, false);
+
+			if ( Look.FollowTransform == null ) {
+				Look.FollowTransform = Camera.main.transform;
+			}
+		}
+
+		/*--------------------------------------------------------------------------------------------*/
+		public void Update() {
+			if ( !InputModuleUtil.FindCursorReference(this, ref CursorDataProvider, true) ) {
+				return;
+			}
+
+			if ( !Application.isPlaying ) {
+				return;
+			}
+
+			CursorDataProvider.MarkAllCursorsUnused();
+
+			Look.UpdateData(CursorDataProvider);
+
+			LeftPalm.UpdateData(CursorDataProvider);
+			LeftThumb.UpdateData(CursorDataProvider);
+			LeftIndex.UpdateData(CursorDataProvider);
+			LeftMiddle.UpdateData(CursorDataProvider);
+			LeftRing.UpdateData(CursorDataProvider);
+			LeftPinky.UpdateData(CursorDataProvider);
+
+			RightPalm.UpdateData(CursorDataProvider);
+			RightThumb.UpdateData(CursorDataProvider);
+			RightIndex.UpdateData(CursorDataProvider);
+			RightMiddle.UpdateData(CursorDataProvider);
+			RightRing.UpdateData(CursorDataProvider);
+			RightPinky.UpdateData(CursorDataProvider);
+
+			CursorDataProvider.ActivateAllCursorsBasedOnUsage();
+		}
+
+	}
+
+}
