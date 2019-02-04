287c0810acadf55209a28a04476df4fba4f5c49c
Zach Kinstner <zach@aestheticinteractive.com>
2019-02-04 18:41:30 -0500

    Added an "auto-fire" helper component that continually reselects an item (at a specified time interval) if the cursor continues hovering on it.

---
+++ Unity/Assets/Hover/Core/Scripts/Items/Helpers/HoverSelectorItemAutoFire.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using Hover.Core.Items.Managers;
+using Hover.Core.Items.Types;
+using UnityEngine;
+
+namespace Hover.Core.Items.Helpers {
+
+	/*================================================================================================*/
+	[RequireComponent(typeof(HoverItemDataSelectable))]
+	public class HoverSelectorItemAutoFire : MonoBehaviour {
+
+		[Range(0, 1)]
+		public float DelaySeconds = 0.25f;
+
+		private WaitForSeconds vWaitForSec;
+		private float vPrevDelaySec;
+
+
+		////////////////////////////////////////////////////////////////////////////////////////////////
+		/*--------------------------------------------------------------------------------------------*/
+		public void Awake() {
+			vPrevDelaySec = -1;
+
+			HoverItemDataSelectable selItem = GetComponent<HoverItemDataSelectable>();
+			selItem.OnSelectedEvent.AddListener(i => StartCoroutine(UpdateForAutoFire(i)));
+		}
+
+
+		////////////////////////////////////////////////////////////////////////////////////////////////
+		/*--------------------------------------------------------------------------------------------*/
+		private IEnumerator UpdateForAutoFire(IItemDataSelectable pSelItem) {
+			if ( DelaySeconds != vPrevDelaySec ) {
+				vWaitForSec = new WaitForSeconds(DelaySeconds);
+				vPrevDelaySec = DelaySeconds;
+			}
+
+			yield return vWaitForSec;
+
+			HoverItemData item = (HoverItemData)pSelItem;
+			item.GetComponent<HoverItemSelectionState>().ReleaseSelectionPrevention();
+		}
+
+	}
+
+}
