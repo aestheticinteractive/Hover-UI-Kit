47dda4664235f1c0ddae980c98c72f666239a001
Zach Kinstner <zach@aestheticinteractive.com>
2019-02-04 17:48:17 -0500

    Added optimizations ot the TreeUpdater class (avoiding excess sibling/child checks, not calling Update() on disabled items). Added a "TreeUpdater.UseLateUpdate" option (can be used when the item needs to wait for other properties to be fully set).

--- Unity/Assets/Hover/Core/Scripts/Utils/TreeUpdater.cs
+++ Unity/Assets/Hover/Core/Scripts/Utils/TreeUpdater.cs
@@ -10,13 +10,20 @@ namespace Hover.Core.Utils {
 		//NOTE: use this with renamed ITreeUpdateable "TreeUpdate()" => "Update()"
 		//private const bool IsProfilingMode = false;
 
+		//TODO:BUG: move tree child out of parent's hierarchy, the child's "TreeParentThisFrame" 
+		//... does not get reset until/unless disable-then-reabling the former parent
+
 		public bool DidTreeUpdateThisFrame { get; private set; }
 		public TreeUpdater TreeParentThisFrame { get; private set; }
 		public int TreeDepthLevelThisFrame { get; private set; }
 		public List<ITreeUpdateable> TreeUpdatablesThisFrame { get; private set; }
 		public List<TreeUpdater> TreeChildrenThisFrame { get; private set; }
+		public bool ReloadTreeChildrenOnUpdate { get; set; }
+
+		public bool UseLateUpdate = false;
 
 		private bool vIsDestroyed;
+		private bool vShouldBeginOnLateUpdate;
 		
 		
 		////////////////////////////////////////////////////////////////////////////////////////////////
@@ -29,15 +36,29 @@ namespace Hover.Core.Utils {
 
 		////////////////////////////////////////////////////////////////////////////////////////////////
 		/*--------------------------------------------------------------------------------------------*/
+		public void OnEnable() {
+			FindTreeChildren();
+		}
+
+		/*--------------------------------------------------------------------------------------------*/
 		public void Update() {
 			/*if ( IsProfilingMode && Application.isPlaying ) {
 				return;
 			}*/
 
+			if ( ReloadTreeChildrenOnUpdate || !Application.isPlaying ) {
+				ReloadTreeChildrenOnUpdate = !Application.isPlaying;
+				FindTreeChildren();
+			}
+
 			if ( DidTreeUpdateThisFrame ) {
 				return;
 			}
 			
+			if ( Application.isPlaying && TreeParentThisFrame != null ) { //experimental
+				return;
+			}
+
 			AscendTreeOrBegin(true);
 		}
 
@@ -48,6 +69,11 @@ namespace Hover.Core.Utils {
 
 		/*--------------------------------------------------------------------------------------------*/
 		public void LateUpdate() {
+			if ( vShouldBeginOnLateUpdate ) {
+				BeginAtThisTreeLevel();
+				vShouldBeginOnLateUpdate = true;
+			}
+
 			DidTreeUpdateThisFrame = false;
 		}
 
@@ -70,11 +96,15 @@ namespace Hover.Core.Utils {
 		private void AscendTreeOrBegin(bool pFromUpdate) {
 			//if ( pFromUpdate ) { Debug.Log("AscendTreeOrBegin: "+gameObject.name, gameObject); }
 
-			Transform parTx = transform.parent;
-			TreeUpdater parTreeUp = (parTx == null ? null : parTx.GetComponent<TreeUpdater>());
+			TreeUpdater parTreeUp = TreeParentThisFrame;
 			
 			if ( parTreeUp == null || !parTreeUp.isActiveAndEnabled ) {
-				BeginAtThisTreeLevel();
+				if ( !UseLateUpdate || !Application.isPlaying ) {
+					BeginAtThisTreeLevel();
+				}
+				else {
+					vShouldBeginOnLateUpdate = true;
+				}
 				return;
 			}
 			
@@ -101,11 +131,20 @@ namespace Hover.Core.Utils {
 				return;
 			}
 
-			gameObject.GetComponents(TreeUpdatablesThisFrame);
-			FindTreeChildren();
+			/*if ( Application.isPlaying ) { //comment out TreeUpdate() call below for profiling
+				SendMessage("TreeUpdate", SendMessageOptions.DontRequireReceiver); //for profiling
+			}*/
 
 			for ( int i = 0 ; i < TreeUpdatablesThisFrame.Count ; i++ ) {
 				ITreeUpdateable treeUp = TreeUpdatablesThisFrame[i];
+
+				if ( treeUp == null ) {
+					if ( !ReloadTreeChildrenOnUpdate ) {
+						ReloadTreeChildrenOnUpdate = true;
+						Debug.Log("Lost tree sibling, will refresh list next frame: "+i, this);
+					}
+					continue;
+				}
 
 				if ( !treeUp.isActiveAndEnabled ) {
 					continue;
@@ -120,7 +159,15 @@ namespace Hover.Core.Utils {
 
 		/*--------------------------------------------------------------------------------------------*/
 		private void FindTreeChildren() {
-			TreeChildrenThisFrame.Clear();
+			if ( TreeChildrenThisFrame.Count > 0 ) {
+				TreeChildrenThisFrame.Clear();
+			}
+
+			if ( transform.parent != null && !transform.parent.gameObject.activeInHierarchy ) {
+				return; //experimental
+			}
+
+			gameObject.GetComponents(TreeUpdatablesThisFrame);
 
 			int childCount = transform.childCount;
 			
@@ -145,10 +192,24 @@ namespace Hover.Core.Utils {
 				return;
 			}
 
+			int childCount = TreeChildrenThisFrame.Count;
 			int childDepth = pDepth+1;
-			
-			for ( int i = 0 ; i < TreeChildrenThisFrame.Count ; i++ ) {
+
+			for ( int i = 0 ; i < childCount ; i++ ) {
 				TreeUpdater childTreeUp = TreeChildrenThisFrame[i];
+
+				if ( childTreeUp == null ) {
+					if ( !ReloadTreeChildrenOnUpdate ) {
+						ReloadTreeChildrenOnUpdate = true;
+						Debug.Log("Lost tree child, will refresh list next frame: "+i, this);
+					}
+					continue;
+				}
+
+				if ( !childTreeUp.isActiveAndEnabled ) {
+					continue;
+				}
+
 				childTreeUp.SendTreeUpdates(childDepth);
 				childTreeUp.DescendTree(childDepth);
 			}
